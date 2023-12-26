e956afb973a2ca49e428f5430a511141494f0252
Leonardo Nicola <leonardonicolares@gmail.com>
2023-12-26 03:00:15 -0300

    transparent

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -4,11 +4,17 @@ return {
     lazy = true,
     opts = {
       style = "night",
+      transparent = true,
       styles = {
-        sidebars = { "qf", "vista_kind", "terminal", "packer" },
+        sidebars = "transparent",
+        floats = "transparent",
         functions = { bold = true },
         keywords = { bold = true, italic = false },
       },
+      on_colors = function(colors)
+        colors.bg_statusline = colors
+        .none                              -- To check if its working try something like "#ff00ff" instead of colors.none
+      end,
     },
   },
   {
