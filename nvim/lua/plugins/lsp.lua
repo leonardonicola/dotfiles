1bc77cabc790ccabf44407dc0315e8a1e71495d6
Leonardo Nicola <leonardonicolares@gmail.com>
2024-03-05 16:58:59 -0300

    remove unused plugins

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,6 +4,7 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "tailwindcss-language-server",
+        "eslint-lsp",
         "typescript-language-server",
         "vue-language-server",
       })
