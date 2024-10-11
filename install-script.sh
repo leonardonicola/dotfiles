04e37876c2fdd4f3b7ad38f7c8a4a4351760fa32
Leonardo Nicola <leonardonicolares@gmail.com>
2024-10-11 04:28:12 -0300

    feat(all): add swaync, new hyprland configs, new install script, new dev pkgs

--- install-script.sh
+++ install-script.sh
@@ -1,18 +1,28 @@
-#!/usr/bin/env zsh
-
-# Update system and install packages
-sudo pacman -Syu --noconfirm
-sudo pacman -S --noconfirm neofetch neovim nodejs htop tldr tmux lua zsh fd ripgrep base-devel fzf zoxide go docker starship
+#!/usr/bin/sh
 
 # Set the path to your dotfiles repository
 dotfiles_path="$(pwd)"
 
-# Link zsh 
-ln -sf "$dotfiles_path/.zshrc" ~/.zshrc
+# Update system and install packages
+sudo pacman -Syu --noconfirm
+
+# Packages
+sudo pacman -S --noconfirm neofetch neovim nodejs btop tldr \
+  tmux lua zsh fd ripgrep base-devel fzf zoxide go \
+  docker starship eza ttf-jetbrains-mono-nerd noto-fonts-emoji \ 
+  waybar pavucontrol nwg-look thunar swaync hyprpaper npm pnpm \
+  git github-cli
 
 # Install Oh My Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 
+# Link zsh 
+ln -sf "$dotfiles_path/.zshrc" ~/.zshrc
+
+# Set zsh to default shell
+chsh -s $(which zsh)
+
+# Zsh plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
@@ -26,14 +36,45 @@ export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || pr
 # Install nvm plugins or Node.js versions
 nvm install --lts
 
+# Commitizen
+pnpm add -g commitizen cz-conventional-changelog
+
+echo '{ "path": "cz-conventional-changelog" }' > ~/.czrc
+
 # Configure tmux and TPM plugin
 ln -sf "$dotfiles_path/tmux/.tmux.conf" ~/.tmux.conf
 git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+# Hyprland
+rm -rf ~/.config/hypr && ln -sf "$dotfiles_path/.config/hypr" ~/.config/hypr
+
+# Alacritty
+ln -sf "$dotfiles_path/.config/alacritty.toml" ~/.alacritty.toml
+
+# Waybar
+ln -sf "$dotfiles_path/.config/waybar" ~/.config/waybar
+
+# Nvim
+ln -sf "$dotfiles_path/.config/nvim" ~/.config/nvim
+
+# Fonts
+ln -sf "$dotfiles_path/.config/fontconfig" ~/.config/fontconfig
+
+# Btop
+ln -sf "$dotfiles_path/.config/btop" ~/.config/btop
+
+# Swaync
+ln -sf "$dotfiles_path/.config/swaync" ~/.config/swaync
+
+# Update fonts
+fc-cache --really-force
+
+# Update zsh
 source ~/.zshrc
 
 echo "✅ Dotfiles setup complete!"
 echo "---------------------------"
 echo -e "Don't forget to: \n"
-echo "1 - Run tmux and install the plugins with <C-b> + I"
-echo "2 - Run nvim and run :Lazy sync and :MasonInstallAll"
+echo "1 - Reboot the system in order to reflect changes"
+echo "2 - Run tmux and install the plugins with <C-b> + I"
+echo "3 - Run nvim and run :Lazy sync and :MasonInstallAll"
